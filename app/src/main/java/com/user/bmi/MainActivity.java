bf19fab9b07d71867be5dbfc149d822ec0f1199c
tt8871720 <tt8871720@gmail.com>
2018-10-02 16:28:15 +0800

    Lod.d , getStringById , bmi

--- app/src/main/java/com/user/bmi/MainActivity.java
+++ app/src/main/java/com/user/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.user.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){//view要import
+        Log.d("Mainactivity" , "bmi");
+        EditText edweight = findViewById(R.id.ed_wieght);
+        EditText edheight = findViewById(R.id.ed_height);
+        String w = edweight.getText().toString();
+        String h = edheight.getText().toString();
+        Log.d("MainActivity"  , w + "/" + h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        Log.d("MainActivity" ,  bmi + "");//畫面還不會顯示
+
+
     }
 }
